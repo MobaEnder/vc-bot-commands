b09ed1936e711460d524bd52627d6172183b14e7
Tai Tran <107969594+MobaEnder@users.noreply.github.com>
2025-12-10 00:36:19 +0700

    Update market.js

--- commands/market.js
+++ commands/market.js
@@ -8,12 +8,13 @@ module.exports = {
     async execute(interaction) {
 
         const embed = new EmbedBuilder()
-            .setTitle("🔗 Arbitrations Guide Links & Support")
+            .setTitle("🔗 Warframe Market Guide")
             .setColor(0xC00000)
             .setDescription(
                 "**🎬 Video Hướng Dẫn Đăng Ký Warframe Market:**\n" +
-                "[Youtube](https://www.youtube.com/watch?v=iK88s1mmIco)\n"
-            );
+                "[YouTube](https://www.youtube.com/watch?v=iK88s1mmIco)\n"
+            )
+            .setImage("https://img.youtube.com/vi/iK88s1mmIco/maxresdefault.jpg"); // preview video
 
         await interaction.reply({ embeds: [embed] });
     }
